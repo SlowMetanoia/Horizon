96da5174f3371772853b14240c9baaa5cd4f3773
Alice <alexandervtarasov@gmail.com>
2022-09-28 15:10:07 +0300

    InfDef 1,2

--- src/main/scala/InfomationDefence/GronsfeldEncode.scala
+++ src/main/scala/InfomationDefence/GronsfeldEncode.scala
@@ -1,31 +1,38 @@
 package InfomationDefence
 
+import scala.io.StdIn
 
 
 object GronsfeldEncode extends App{
+  //Ключ
   class CodingKey(key:String){
-    def repeat(key:String):LazyList[String] = key#::repeat(key)
-    def fullKey(key:String):LazyList[Char] = repeat(key).flatten
-    def codingStructure(text:String): Seq[(Char, Char)] = text.zip(fullKey(key).take(text.length))
+    //бесконечная последовательность значений равных ключу
+    def repeat:LazyList[String] = key#::repeat
+    //Бесконечно повторяющаяся последовательность символов ключа
+    def fullKey:LazyList[Char] = repeat.flatten
+    //результат сцепления соответствующих элементов последовательности
+    def codingStructure(text:String): Seq[(Char, Char)] = text.zip(fullKey.take(text.length))
   }
-  def encode(alfaBet: CodingKey,text:String): String =
-    alfaBet.codingStructure(text).map{case (symbol,shift) => (symbol+shift).toChar}.mkString
-  
-  def decode(alfaBet: CodingKey,text:String): String =
-    alfaBet.codingStructure(text).map{case (symbol,shift) => (symbol-shift).toChar}.mkString
-    
-  val codingKey = new CodingKey("123456783456345")
-  
-  val text = "Not useless murderer"
-  
-  println(text)
-  
-  val encoded = encode(codingKey,text)
-  
+  //закодировать текст
+  def encode(key: CodingKey, text:String): String =
+    key.codingStructure(text).map{case (symbol,shift) => (symbol+shift).toChar}.mkString
+  //декодировать текст
+  def decode(key: CodingKey, text:String): String =
+    key.codingStructure(text).map{case (symbol,shift) => (symbol-shift).toChar}.mkString
+
+
+
+  println("Input key:")
+  val key = new CodingKey(StdIn.readLine)
+
+  println("Input source text:")
+  val sourceText = StdIn.readLine()
+
+  println("encoded text:")
+  val encoded = encode(key,sourceText)
   println(encoded)
-  
-  val decoded = decode(codingKey,encoded)
-  
+
+  println("decoded text:")
+  val decoded = decode(key,encoded)
   println(decoded)
-  
 }
