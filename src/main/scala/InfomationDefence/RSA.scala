cd310d69440b9e2b01fd7a651ba2d6799b5bf4aa
Alice <alexandervtarasovddd@gmail.com>
2022-09-30 13:49:15 +0300

    ECB - it seems to be almost working. RSA - just file created.

--- src/main/scala/InfomationDefence/RSA.scala
+++ src/main/scala/InfomationDefence/RSA.scala
@@ -1,6 +1,12 @@
 package InfomationDefence
 
 object RSA extends App{
+  //ряды
+  def series[T](prev:T)(next:T=>T):LazyList[T] = prev#::series(next(prev))(next)
+  //решето Эратосфена
+  def Erotosthenes(n:Long):Seq[Long] = {
+    ???
+  }
   def generatePrimary(n:Int):BigInt = {
   ???
   }
